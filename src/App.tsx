a673443c1d7f6fc634ad686d60a6f9aa71ce4856
OlgaTabisheva <93075832+OlgaTabisheva@users.noreply.github.com>
2022-09-08 22:12:19 +0300

    преложение погоды - в процессе работы

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,21 @@ import {Route, Routes} from 'react-router';
 import Home from "./pages/Home/Home";
 import MonthStatistics from "./pages/MonthStatistics/MonthStatistics";
 import Header from "./shared/Header/Header";
+import Popup from "./shared/Popup/Popup";
 
 function App() {
     return (
+<div className='global-container'>
+        <Popup/>
         <div className="container">
+
             <Header/>
             <Routes>
                 <Route path='/' element={<Home/>}/>
                 <Route path='/month-statistics' element={<MonthStatistics/>}/>
             </Routes>
         </div>
+</div>
     );
 }
 
